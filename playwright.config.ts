7fb2cafc37af87955657758b60fb465989b1b7fb
M Abbas Khan <111870160+Ak-mohmnd@users.noreply.github.com>
2024-12-05 12:59:41 +0500

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,6 +23,8 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    headless: process.env.PLAYWRIGHT_HEADLESS !== 'false', // Dynamically respect the environment variable
+
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
